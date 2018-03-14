6bb1b9848130a36971cae129612fc098d3e9775f
Nathan Daly <nhdaly@gmail.com>
2018-03-13 23:16:18 -0500

    Added unit tests for SDL_mixer.jl.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,8 @@ include("lib/SDL.jl")
 
 include("lib/SDL_ttf.jl")
 
+include("lib/SDL_mixer.jl")
+
 # Integration tests
 @testset "example1" begin
 include(joinpath("..","src","examples","example1_cairo.jl"))
